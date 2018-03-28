d8795f4b3fc69f85a956569ddc212c6f998bebea
gaomy15805 <gaomy15805@hundsun.com>
2018-03-28 09:48:57 +0800

    注释代码放开

--- Code/mulitithread/src/test/java/MultiThreadTest.java
+++ Code/mulitithread/src/test/java/MultiThreadTest.java
@@ -5,11 +5,11 @@ import java.util.concurrent.Executors;
 public class MultiThreadTest {
 
     public static void main(String[] args) throws InterruptedException {
-//        MultiThread thread = new MultiThread();
-//        for (int i=1;i<100;i++) {
-//            thread.ImitateUser("第"+i+"节点");
-//            Thread.sleep(2000);
-//        }
+        MultiThread thread = new MultiThread();
+        for (int i=1;i<100;i++) {
+            thread.ImitateUser("第"+i+"节点");
+            Thread.sleep(2000);
+        }
 
     }
 
