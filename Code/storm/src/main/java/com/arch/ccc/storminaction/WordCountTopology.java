c4f8bcd7cc59d08ef3eec25ef55ddffe1994eaee
banana1995 <1770261049@qq.com>
2019-07-04 10:07:42 +0800

    google paper 提交

---
+++ Code/storm/src/main/java/com/arch/ccc/storminaction/WordCountTopology.java
@@ -0,0 +1,16 @@
+package com.arch.ccc.storminaction;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class WordCountTopology {
+
+    private static final String SENTENCE_SPOUT_ID = "sentence-spout";
+    private static final String SPLIT_BOLT_ID = "split-bolt";
+    private static final String COUNT_BOLT_ID = "count-bolt";
+    private static final String REPORT_BOLT_ID = "report-bolt";
+    private static final String TOPOLOGY_NAME = "word-count-topology";
+
+   
+
+}
