45c79f032342069dfbc5617866d7434370aa233e
Banana1995 <1770261049@qq.com>
2020-03-22 22:31:26 +0800

    ddia文档修改提交

---
+++ Code/mulitithread/src/main/java/com/banana/multithread/fori.java
@@ -0,0 +1,15 @@
+package com.banana.multithread;
+
+public class fori {
+
+    public void teFor() {
+        for (; ; ) {
+            int i = 0;
+        }
+    }
+    public void teWhile() {
+        while (true) {
+            int i = 0;
+        }
+    }
+}
