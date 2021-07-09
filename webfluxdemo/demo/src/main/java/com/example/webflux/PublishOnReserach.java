73b61941183df12e66b363a228f069cd7138060e
Banana1995 <1770261049@qq.com>
2021-07-09 21:34:35 +0800

    action demo

---
+++ webfluxdemo/demo/src/main/java/com/example/webflux/PublishOnReserach.java
@@ -0,0 +1,37 @@
+package com.example.webflux;
+
+import static java.lang.Thread.sleep;
+
+import java.util.ArrayList;
+import java.util.Random;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class PublishOnReserach {
+
+
+  public static void main(String[] args) throws InterruptedException {
+    final Random random = new Random();
+    Flux.generate(ArrayList::new, (list, sink) -> {
+      int value = random.nextInt(100);
+      list.add(value);
+      System.out.println("发射元素产生的线程为：" + Thread.currentThread().getName());
+      sink.next(value);
+      try {
+        sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      if (list.size() == 200000) {
+        sink.complete();
+      }
+      return list;
+    }).publishOn(Schedulers.boundedElastic(), 2)
+        .map(x -> String.format("[%s]  %s", Thread.currentThread().getName(), x))
+
+        .subscribe(System.out::println);
+
+    sleep(20000);
+  }
+
+}
